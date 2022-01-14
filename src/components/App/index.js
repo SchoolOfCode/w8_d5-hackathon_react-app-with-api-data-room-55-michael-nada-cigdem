c446c547b86fd3bc631dc81d8409965222be9b41
Your Name <you@example.com>
2022-01-14 12:23:54 +0000

    app ready for first deploy

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,23 @@
 import "./App.css";
 import VideoViewer from "../VideoViewer";
 import ReactPlayer from "react-player";
+import {useState} from 'react'
 // import Vimeo from "vimeo";
 
 function App() {
+  const [randomNumber, setrandomNumber] = useState(0);
+  
+  function getRandomNumber() {
+    const number = Math.floor(Math.random() * 20 + 1);
+    setrandomNumber(number);
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <VideoViewer></VideoViewer>
+        <button onClick={getRandomNumber}>Random Generator</button>
+        <VideoViewer randomNumber={randomNumber}></VideoViewer>
+        <VideoViewer randomNumber='5'></VideoViewer>
       </header>
     </div>
   );
--- src/components/VideoViewer/index.js
+++ src/components/VideoViewer/index.js
@@ -1,40 +1,36 @@
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 
-function VideoViewer() {
+function VideoViewer({randomNumber}) {
   const [video, setVideo] = useState("");
+  
   console.log("VIDEO");
   console.log(video);
 
   async function fetchRandomVideos() {
-    console.log("FATCH RANDOM VIDEOS");
-    console.log({ video });
-
     const response = await fetch(
       "https://v1.nocodeapi.com/bingbong/vimeo/DKRJHhGNcGRIluxb/search?q=funny&page=2&perPage=300"
     );
     const data = await response.json();
-    console.log(data);
+    console.log("THIS IS OUR DATA",data);
     setVideo(data);
   }
-
-  const fetch = fetchRandomVideos();
-  console.log(fetch);
+  
 
   useEffect(
     function () {
-      setVideo();
+      fetchRandomVideos();
     },
-    [video]
+    []
   );
 
   return video ? (
     <div id="video-viewer">
       <p>
         This is the name of your video
-        <b> {video.data[0].name}</b>
+        <b> {video.data[randomNumber].name}</b>
       </p>
-      <ReactPlayer url={video.data[0].link} />
+      <ReactPlayer url={video.data[randomNumber].link} />
     </div>
   ) : (
     <></>
